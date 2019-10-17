8f58b9726d15ace1aaf635fc202bd07b9ab6395f
anudeep <anudeepstty@gmail.com>
2019-10-17 22:52:40 +0530

    Done

--- script.py
+++ script.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def about():
-    return render_template("about.html")
-
-@app.route('/about/')
 def home():
     return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
