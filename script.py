95160ae8c91eb709b28afeb8183adb2f82cde74d
anudeep <anudeepstty@gmail.com>
2019-10-20 00:07:16 +0530

    UI changes

--- script.py
+++ script.py
@@ -9,6 +9,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
